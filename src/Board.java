f46c7f4da1bbfc789e2d147b8b88a1561b67315e
Matthew Renze <merenze@gmail.com>
2016-05-18 22:41:28 -0500

    Add board.victory()

--- src/Board.java
+++ src/Board.java
@@ -41,32 +41,46 @@ public class Board {
 		}
 		return true;
 	}
+	public boolean victory() {
+		for (int row=0;row<=3;row++) {
+			for (int col=0;col<=3;col++) {
+				if (board[row][col].getValue()==2048) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 	//Mutators
 	public void spawnTile() {
 		java.util.Random random = new java.util.Random();
 		int row = random.nextInt(4);
 		int col = random.nextInt(4);
 		boolean valid = board[row][col]==null;
-		while (!valid) {
-			if (row<3) {
-				row++;
-			} else {
-				row=0;
-				if (col<3) {
-					col++;
+		if (!isFull()) {
+			while (!valid) {
+				if (row<3) {
+					row++;
 				} else {
-					col=0;
+					row=0;
+					if (col<3) {
+						col++;
+					} else {
+						col=0;
+					}
+				}
+				System.out.printf("Attempting to spawn tile at [%d][%d]\n",row,col);
+				try {
+					valid=board[row][col]==null;
+				} catch (ArrayIndexOutOfBoundsException ex) {
+					System.out.printf("Array index out of bounds: [%d][%d]\n", row,col);
 				}
 			}
-			System.out.printf("Attempting to spawn tile at [%d][%d]\n",row,col);
-			try {
-				valid=board[row][col]==null;
-			} catch (ArrayIndexOutOfBoundsException ex) {
-				System.out.printf("Array index out of bounds: [%d][%d]\n", row,col);
-			}
+			board[row][col]=new Tile();
+			System.out.printf("Spawning new tile at [%d][%d]\n",row,col);
+		} else {
+			System.out.println("Failed to spawn new tile: Board full");
 		}
-		board[row][col]=new Tile();
-		System.out.printf("Spawning new tile at [%d][%d]",row,col);
 	}
 	//Moves
 	public void up() {
